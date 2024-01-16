3f61a51c08a9482b5e3bf70bba1910d8dc4e71b9
Lukasz <lukasz_domagala@interia.pl>
2024-01-16 22:20:18 +0100

    Setting page demo

---
+++ frontend/src/assets/icons/ArrowDown.tsx
@@ -0,0 +1,12 @@
+import IconBase from "./IconBase";
+
+const ArrowDown = () => {
+	return (
+		<IconBase>
+            <path fill="none" d="M0 0h24v24H0V0z"></path>
+            <path d="M7.41 8.59 12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"></path>
+		</IconBase>
+	);
+};
+
+export default ArrowDown;
--- frontend/src/pages/SettingsPage.tsx
+++ frontend/src/pages/SettingsPage.tsx
@@ -4,9 +4,38 @@ import LogoutIcon from "../assets/icons/LogoutIcon";
 import { LOGIN, NOTES } from "../consts";
 import NotesOutlineIcon from "../assets/icons/NotesOutlineIcon";
 
+
+
+
 const SettingsPage = () => {
+
 	return (
-		<div className="min-h-screen">
+		<div className="min-h-screen hero">
+			<div className="hero-content flex-col">
+			<div>$USERNAME$</div>
+			<div className="card w-96 bg-neutral text-neutral-content ">
+				<div className="card-body items-center text-center p-10">
+					<h1 className="card-title">Change Password</h1>
+					<input type="password" placeholder="New Password" className="input input-bordered w-full max-w-xs" />
+					<input type="password" placeholder="Repeat Password" className="input input-bordered w-full max-w-xs" />
+
+					<div className="card-actions justify-end w-full">
+						<button className="btn btn-primary">Accept</button>
+					</div>
+				</div>
+			</div>
+			<div className="card w-96 bg-neutral text-neutral-content">
+				<div className="collapse border border-error collapse-arrow">
+					<input type="checkbox" className="w-full" />
+					<div className="collapse-title flex">
+						DANGER ZONE ! 
+					</div>
+					<div className="collapse-content justify-center flex">
+						<button className="btn btn-error">Delete Account</button>
+					</div>
+				</div>
+			</div>
+			</div>
 			<div className="absolute flex flex-col top-5 right-5 gap-4">
 				<div className="tooltip tooltip-left" data-tip="Theme">
 					<ThemeButton />
